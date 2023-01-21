9037d716a2f9dc35d20c7fbdff9a949eafa739b1
Marek Więcaszek <m.wiecaszek.446@studms.ug.edu.pl>
2023-01-21 19:32:39 +0100

    CRUD for games and users

--- Projekt/routes/games.js
+++ Projekt/routes/games.js
@@ -1,17 +1,131 @@
-// game_id
-// title
-// developer
-// category
-// age_restricted
-// description
-// release_year
-// score (AVG z powiązanych comments)
+
 
 const express = require('express');
 const router = express.Router({mergeParams: true});
 const driver = require('../config/neo4jDriver');
 const session = driver.session();
 
+router.get('/', async(req, res) => {
+    const session = driver.session();
+    const games = []
+    await session
+    .run(`MATCH (n:Game) RETURN n`)
+    .subscribe({
+        onNext: async record => {
+            const result = await record.get('n').properties
+            const id = await record.get('n').elementId
+            const scores = result.comments.map(x => {
+                x.score
+            })
+            const score = scores.reduce((a, b) => a + b , 0) / scores.length
+            games.push({"id": id, ...result, "score": score})
+        },
+        onCompleted: () => {
+            session.close()
+            res.send({"games": games})
+        },
+        onError: error => {
+            console.log(error);
+            res.send("Sth went wrong")
+        }
+    })
+});
+
+router.get('/:id', async (req, res) => {
+    const session = driver.session();
+    let result = "Not found"
+    await session
+      .run(`MATCH (n:Game) where id(n) = ${req.params.id} RETURN n`)
+      .subscribe({
+        onNext: async record => {
+            const game = await record.get('n').properties
+            const id = await record.get('n').elementId
+            
+            const scores = game.comments.map(x => {
+                x.score
+            })
+            const score = scores.reduce((a, b) => a + b , 0) / scores.length
+            result = {"id": id, ...game, "score": score}
+        },
+        onCompleted: () => {
+          session.close();      
+          res.send({"game": result});
+        },
+        onError: error => {
+          console.log(error)
+        },
+      })  
+  });
+
+router.post('/create', async (req, res) => {
+    const session = driver.session();
+    const game = req.body
+    await session
+      .run(`CREATE (n:Game {title: "${game.title}",
+            image_url: "${game.image_url}",
+            developer: "${game.developer}",
+            category: "${game.category}",
+            age_restricted: "${game.age_restricted}",
+            description: "${game.description}",
+            release_year: "${game.release_year}",
+            comments: [] })`)
+      .subscribe({
+        onCompleted: () => {
+          session.close();      
+          res.send("success")
+        },
+        onError: error => {
+          console.log(error)
+          res.send(error)
+        },
+      })  
+})
+
+router.put('/:id', async (req, res) => {
+    const game = req.body
+    const session = driver.session();
+        await session
+      .run(`MATCH (n:Game) WHERE id(n)=${req.params.id}
+        SET n.title = "${game.title}"
+        SET n.image_url = "${game.image_url}"
+        SET n.developer = "${game.developer}"
+        SET n.category = "${game.category}"
+        SET n.age_restricted = "${game.age_restricted}"
+        SET n.description = "${game.description}"
+        SET n.release_year = "${game.release_year}"`)
+      .subscribe({
+        onCompleted: () => {
+          session.close();      
+          res.send("success")
+        },
+        onError: error => {
+          console.log(error)
+          res.send(error)
+        },
+      })  
+    
+})
+
+router.delete('/:id', async (req, res) => {
+    const session = driver.session();
+    await session
+    .run(`Match (n: Game) where id(n) = ${req.params.id}
+    DETACH DELETE n`)
+    return res.send("Deleted");
+})
+
+// zwracanie score jako avg
+//
+
+// game_id
+// title
+// image_url
+// developer
+// category
+// age_restricted
+// description
+// release_year
+// comments (z powiązanych comments)
 
 
 module.exports = router;
--- Projekt/routes/users.js
+++ Projekt/routes/users.js
@@ -1,10 +1,3 @@
-// username
-// password
-// age
-// e-mail
-// address
-// phone_number
-// [game_id's] (Favourites)
 
 const express = require('express');
 const router = express.Router({mergeParams: true});
@@ -12,8 +5,144 @@ const driver = require('../config/neo4jDriver');
 const session = driver.session();
 
 router.get('/', async(req, res) => {
-    res.send("Working")
+    const session = driver.session();
+    const users = []
+    await session
+    .run(`MATCH (n:User) RETURN n`)
+    .subscribe({
+        onNext: async record => {
+            const result = await record.get('n').properties
+            const id = await record.get('n').elementId
+            users.push({"id": id, ...result})
+        },
+        onCompleted: () => {
+            session.close()
+            res.send({"users": users})
+        },
+        onError: error => {
+            console.log(error);
+            res.send("Sth went wrong")
+        }
+    })
+});
+
+router.get('/:id', async (req, res) => {
+    const session = driver.session();
+    let result = "Not found"
+    await session
+      .run(`MATCH (n:User) where id(n) = ${req.params.id} RETURN n`)
+      .subscribe({
+        onNext: async record => {
+            const user = await record.get('n').properties
+            const id = await record.get('n').elementId
+            result = {"id": id, ...user}
+        },
+        onCompleted: () => {
+          session.close();      
+          res.send({"user": result});
+        },
+        onError: error => {
+          console.log(error)
+        },
+      })  
+  });
+
+router.post('/create', async (req, res) => {
+    const session = driver.session();
+    const user = req.body
+    await session
+      .run(`CREATE (n:User {username: "${user.username}",
+            password: "${user.password}",
+            age: "${user.age}",
+            email: "${user.email}",
+            phone_number: "${user.phone_number}",
+            address: "${user.address}" })`)
+      .subscribe({
+        onCompleted: () => {
+          session.close();      
+          res.send("success")
+        },
+        onError: error => {
+          console.log(error)
+          res.send(error)
+        },
+      })  
 })
+
+router.put('/:id', async (req, res) => {
+    const old_username = req.body.old_username
+    const passowrd = req.body.old_password
+    const user = req.body
+    const session = driver.session();
+
+    const return_result = async () => {
+        const session = driver.session();
+        await session
+      .run(`MATCH (n:User) WHERE id(n)=${req.params.id}
+            SET n.username = "${user.username}"
+            SET n.password = "${user.password}"
+            SET n.age = "${user.age}"
+            SET n.email = "${user.email}"
+            SET n.phone_number = "${user.phone_number}"
+            SET n.address = "${user.address}"`)
+      .subscribe({
+        onCompleted: () => {
+          session.close();      
+          res.send("success")
+        },
+        onError: error => {
+          console.log(error)
+          res.send(error)
+        },
+      })  
+    }
+
+    let user_in_db = "Not found"
+   
+     await session
+        .run(`MATCH (n:User) where id(n) = ${req.params.id} RETURN n`)
+        .subscribe({
+            onNext: async (record) => {
+                const data = await record.get('n').properties;
+                const id = await record.get('n').elementId;
+                user_in_db = { "id": id, ...data };
+                console.log(user_in_db);
+            },
+            onCompleted: async () => {
+                await session.close();
+
+                if (user_in_db.username !== old_username) {
+                    return res.send("Incorrect user data");
+                }
+                if (user_in_db.password !== passowrd) {
+                    return res.send("Incorrect user data");
+                }
+                return return_result()
+            },
+            onError: error => {
+                console.log(error);
+            },
+        })
+})
+
+router.delete('/:id', async (req, res) => {
+    const session = driver.session();
+    await session
+    .run(`Match (n: User) where id(n) = ${req.params.id}
+    DETACH DELETE n`)
+    return res.send("Deleted");
+})
+
+// dodanie/usuniecie (relacji) favourite
+// pobranie favourites
+
+// username
+// password
+// age
+// e-mail
+// address
+// phone_number
+// [game_id's] (Favourites)
 
 
 
